99bfbfccbd1b97dfc4551af351984be0113e3bc0
bojandjukic1 <bojan.djukic5@gmail.com>
2016-09-26 22:05:28 +1000

    Straight line code to find the sum of multiples of 3 and 5 up below 1000

--- euler_project1/test.py
+++ euler_project1/test.py
@@ -1,4 +1,16 @@
-# this is a test 
 
+counter = 0;
+factors = [];
+sum = 0;
 
-# this is also a test
+for i in range(1,1000):
+        if (i % 3 == 0 or i % 5 == 0):
+                print (i);
+                factors.append(i);
+
+for j in range(len(factors)):
+        sum += factors[j];
+
+print("sum is", sum);
+
+print(factors)
